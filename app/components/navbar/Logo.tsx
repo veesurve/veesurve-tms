e440f24b84da7af3dbeb729b490ed2bfc6d85e87
Abhishek Kohli <ab02110211@gmail.com>
2024-01-06 00:04:57 +0000

    added Navbar

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo=()=>{
+ const router=useRouter()
+
+ return(
+  <Image
+  alt="logo"
+  className="hidden md:block cursor-pointer"
+  height="75"
+  width="75"
+  src="/images/logo.png"
+  
+  />
+ )
+}
+
+export default Logo
