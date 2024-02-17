af45df62440b41a27d5ef2f74d67d13c6ae8725d
Abhishek Kohli <ab02110211@gmail.com>
2024-02-17 19:01:23 +0000

    added email from Contact Form

--- app/(marketing)/contact/page.tsx
+++ app/(marketing)/contact/page.tsx
@@ -10,8 +10,41 @@ import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 import { BsFillSendFill } from "react-icons/bs";
 import { ImWhatsapp } from "react-icons/im";
 
+import { FormEvent, useEffect, useState } from "react";
+import emailjs from "@emailjs/browser";
+
 const ContactPage = () => {
 	const router = useRouter();
+	const [nameInput, setNameInput] = useState("");
+	const [emailInput, setEmailInput] = useState("");
+	const [phoneInput, setPhoneInput] = useState(0);
+	const [messageInput, setMessageInput] = useState("");
+
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => emailjs.init("ZLHwm2yp3LLR7jdVw"), []);
+
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
+		const serviceId = "service_zdu7tg3";
+		const templateId = "template_cjfne2b";
+		try {
+			setLoading(true);
+			await emailjs.send(serviceId!, templateId!, {
+				name: nameInput,
+				email: emailInput,
+				phone: phoneInput,
+				message: messageInput,
+			});
+			alert("email successfully sent, We'll get back to you soon ");
+		} catch (error) {
+			console.log(error);
+			setLoading(false);
+		} finally {
+			setLoading(false);
+		}
+	};
+	// console.log(nameInput);
 	return (
 		<>
 			<div className="h-full w-full p-5 bg-gray-100 flex flex-col gap-4 items-center mt-[110px]">
@@ -20,43 +53,69 @@ const ContactPage = () => {
 
 				<div
 					id="container"
-					className=" w-full h-[70vh] flex gap-2  items-center md:flex-col "
+					className=" w-full h-full  flex gap-2  items-center flex-col flex-1"
 				>
 					<h1 className="font-extrabold text-lg">Contact Us</h1>
-					<div id="container" className="flex flex-row gap-2 w-full px-1">
+					<div
+						id="wrapper"
+						className="flex flex-col md:flex-row gap-2 w-full px-1"
+					>
 						<div
 							id="left"
-							className="w-3/4 p-3 flex flex-col rounded-md border"
+							className=" w-full md:w-3/4 p-3 flex flex-col rounded-md border"
 						>
 							{/* <h1 className="font-semibold">Drop us a Line!</h1> */}
-							<div id="formContainer">
-								<div id="elemContainer">
+							<form id="formContainer" onSubmit={handleSubmit} className="pb-1">
+								<div id="elemContainer" className="flex flex-col gap-1">
 									<label htmlFor="">Name</label>
-									<InputS type="text" placeholder="Please enter your name" />
+									<InputS
+										onChange={(e) => setNameInput(e.target.value)}
+										type="text"
+										placeholder="Please enter your name"
+									/>
 								</div>
-								<div id="elemContainer">
+								<div id="elemContainer" className="flex flex-col gap-1">
 									<label htmlFor="">Email</label>
-									<InputS type="email" placeholder="Please enter your Email" />
+									<InputS
+										onChange={(e) => setEmailInput(e.target.value)}
+										type="email"
+										placeholder="Please enter your Email"
+									/>
 								</div>
-								<div id="elemContainer">
+								<div id="elemContainer" className="flex flex-col gap-1">
 									<label htmlFor="">Phone</label>
-									<InputS type="tel" placeholder="Please enter your name" />
+									<InputS
+										onChange={(e) => setPhoneInput(Number(e.target.value))}
+										type="tel"
+										pattern="[0-9]{10}"
+										maxLength={10}
+										placeholder="Please enter your Phone"
+									/>
 								</div>
-								<div id="elemContainer">
+								<div id="elemContainer" className="flex flex-col gap-1">
 									<label htmlFor="">Message</label>
-									<Textarea placeholder="Type your message here" id="message" />
+									<Textarea
+										placeholder="Type your message here"
+										id="message"
+										onChange={(e) => setMessageInput(e.target.value)}
+									/>
 								</div>
-								<Button className="w-full mt-3 group border hover:border-neutral-900 rounded-full font-semibold text-md hover:bg-neutral-100 bg-neutral-900 text-white border-white p-2">
+								<Button
+									className="w-full mt-3 group border hover:border-neutral-900 rounded-full font-semibold text-md hover:bg-neutral-100 bg-neutral-900 text-white border-white p-2"
+									disabled={loading}
+									type="submit"
+									form="formContainer"
+								>
 									<p className="mr-2 group-hover:text-neutral-900 text-white">
 										Send Message
 									</p>
 									<BsFillSendFill className="group-hover:animate-ping group-hover:text-neutral-900 transition text-white" />
 								</Button>
-							</div>
+							</form>
 						</div>
 						<div
 							id="right"
-							className="w-1/4 p-3 flex flex-col gap-3 border rounded-lg"
+							className="w-full md:w-1/4 p-3 flex flex-col gap-3 border rounded-lg"
 						>
 							<div className="flex flex-col gap-4 ">
 								<p>
@@ -90,9 +149,14 @@ const ContactPage = () => {
 									</p>
 								</div>
 								<div className="flex flex-row justify-center items-center gap-1">
-									<Button className="group border hover:border-neutral-900 rounded-full font-semibold text-md hover:bg-neutral-100 bg-neutral-900 text-white border-white p-5">
+									<Button
+										className="group border hover:border-neutral-900 rounded-full font-semibold text-md hover:bg-neutral-100 bg-neutral-900 text-white border-white p-[1vw]
+									text-[1vw]
+									sm:text-sm sm:text-wrap
+									"
+									>
 										<ImWhatsapp
-											size={25}
+											size={20}
 											className="group-hover:animate-bounce transition group-hover:text-neutral-900 bg-[#44C554] rounded-full"
 										/>
 										<a
